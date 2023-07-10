f672880b247602fec1cade37bf83629d12f43537
OscarDikma <oscardikma@gmail.com>
2023-07-10 17:02:41 -0300

    login somente com usurio e senha correta

--- src/components/BoxLogin.js
+++ src/components/BoxLogin.js
@@ -1,40 +1,47 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// Importe os ícones específicos que você deseja usar
-import { faLock, faUser } from '@fortawesome/free-solid-svg-icons';
-
-
-
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 
 const BoxLogin = () => {
-    return (
-        <div class = 'form -control'>
-            <div ><h1 style={{ marginBottom: '50px' }}>Queda Zero login</h1></div>
-            <div style={{ display: 'grid', gridTemplateColumns: 'auto 1fr', columnGap: '10px' }}>
-                <div style={{margin: 'auto' }}>
-                    <FontAwesomeIcon icon={faUser} beat style={{ marginRight: '10px' }} />
-                </div>
-                <input type='text' placeholder='Username' />
-                <div>
-                    <FontAwesomeIcon icon={faLock} beat style={{ marginRight: '10px' }} />
-                </div>
-                <input type='password' placeholder='Password' />
-            </div>
+  const [user, setUser] = useState('');
+  const [pass, setPass] = useState('');
 
-            <div> 
-                <button style={{ borderRadius: '20px' }}>Login</button>
-            </div>
+    const handleLogin = () => {
+        if (user === 'admin' && pass === 'dikma') {
+            document.getElementById('login').style.display = 'none'
+            document.getElementById('form').style.display = 'block'
+        }else{
+            alert('Usuario ou senha incorreto, tente novamente!')
+        }
+    };
 
-            <div style={{marginTop:'30px'}}>
-                <input type='checkbox'/>Remember-me
-            </div>
+  return (
+    <div className="form-control">
+      <div>
+        <h1 style={{ marginBottom: '50px' }}>Queda Zero login</h1>
+      </div>
+      <div style={{ display: 'grid', gridTemplateColumns: 'auto 1fr', columnGap: '10px' }}>
+        <div style={{ margin: 'auto' }}>
+          <FontAwesomeIcon icon={faUser} beat style={{ marginRight: '10px' }} />
         </div>
-    )
-}
-/*
-function logar(){
-    document.getElementById('login').style.display='none'
-    document.getElementById('form').style.display='block'
-  
-}*/
-export default BoxLogin
+        <input type="text" placeholder="Username" value={user} onChange={(e) => setUser(e.target.value)} />
+        <div>
+          <FontAwesomeIcon icon={faLock} beat style={{ marginRight: '10px' }} />
+        </div>
+        <input type="password" placeholder="Password" value={pass} onChange={(e) => setPass(e.target.value)} />
+      </div>
+
+      <div>
+        <button style={{ borderRadius: '20px' }} onClick={handleLogin}>
+          Login
+        </button>
+      </div>
+
+      <div style={{ marginTop: '30px' }}>
+        <input type="checkbox" /> Remember-me
+      </div>
+    </div>
+  );
+};
+
+export default BoxLogin;
